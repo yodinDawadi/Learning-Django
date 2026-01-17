43ffaa374119a25159e7f48f0edbc38b0514cb01
yodinDawadi <yodindawadi10@gmail.com>
2026-01-17 21:53:01 +0545

    Learned how to render templates

--- practice/practice/views.py
+++ practice/practice/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def Home(request):
-    return HttpResponse("This is home page")
+    #return HttpResponse("This is home page")
+    
+    return render(request,'index.html')
 
 def about(request):
     return HttpResponse("This is about page")
